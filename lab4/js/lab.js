dad89e62c2ed43fa0bc7982653f6856110de3c88
stellavrooom <91557018+stellavrooom@users.noreply.github.com>
2021-10-13 19:30:48 -0700

    lab2online

---
+++ lab4/js/lab.js
@@ -0,0 +1,16 @@
+// Summary. (use period)
+// Description. (use period)
+// @link   URL
+// @file   This files defines the MyClass class.
+// @author AuthorName.
+// @since  x.x.x
+
+//Playing Music on a Webpage
+  //Open webpage
+  //Fetch files from specified folder
+  //Play music in background
+    //Each page refresh, shuffle song order
+    //Pause music
+    //Skip music
+  //Song name is displayed over a music play bar
+  //Play bar shows the length of each song, pause and play buttons

b35c9b05ee74653cdef31ee8b03741a9c729d93d
stellavrooom <91557018+stellavrooom@users.noreply.github.com>
2021-10-19 10:03:04 -0700

    lab

---
+++ lab5/js/lab.js
@@ -0,0 +1,23 @@
+/**
+ *
+ * Author: Alise Morgan & Stella Brown
+ * Created: 18 October 2021
+ * License: Public Domain
+ *
+ **/
+
+ // Define Variables
+ var make = "Ford";
+ var model = "Ranger";
+ var color = "Silver";
+ var year = 2001;
+
+ // Calculate age
+ var age = 2020 - year;
+
+ // Output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "<br>");

9dfd92d85ce8eb4cb7f13558242bb1671915a759
stellavrooom <91557018+stellavrooom@users.noreply.github.com>
2021-11-01 16:37:03 -0700

    lab9

---
+++ lab9/js/site.js
@@ -0,0 +1,22 @@
+/**
+ * Author: Alise Morgan & Stella Brown
+ * Created: 11/1/21
+ *
+ * Public Domain
+ **/
+
+ var outputEl = document.getElementById("div")
+
+ var new1El =  document.createElement("p")
+      new1El.innerHTML = "paragraph";
+      document.body.appendChild(new1El);
+
+var new2El = document.createElement("b")
+      new2El.innerHTML = "body";
+        document.body.appendChild(new2El);
+
+        document.getElementById("h1").
+        style.color = "purple";
+
+        document.getElementById("h2").
+        style.color = "blue";

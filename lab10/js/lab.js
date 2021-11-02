607d548ff5c24af800af85a274d719e8b4c9f2f6
stellavrooom <91557018+stellavrooom@users.noreply.github.com>
2021-11-02 12:50:42 -0700

    lab10

---
+++ lab10/js/lab.js
@@ -0,0 +1,25 @@
+/**
+ * Author: Alise Morgan & Stella Brown
+ * Created: 11/2/21
+ *
+ * Public Domain
+ **/
+
+
+ function putNameInTitle() {
+  // When the button is pushed,
+  // prompt the user to input their name
+  // with the prompt() function.
+  //var name = prompt("Please enter your name:");
+  var field = document.getElementById("myfield");
+  var name = field.value;
+
+  // Use this input to change the text from
+  // ‘Hello Everyone!!’ to ‘Hello’ + the user’s name
+  var headingEl = document.getElementById("title");
+  headingEl.innerHTML = "Hello, " + name;
+}
+
+// Create an event handler within the JS file that listens for a button click.
+var buttonEl = document.getElementById("mybutton");
+buttonEl.addEventListener("click", putNameInTitle);
